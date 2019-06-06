e653168e18588d54a1f723b95d8d2b8feaf4d24b
GRUBCAVE-WIN\Pharylon <me@zachshuford.com>
2019-06-06 17:55:21 -0400

    Updates

---
+++ precache-manifest.52e82700e117c4787bbdc01c8c187f3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e79b463189effa636951a406342d5c5",
+    "url": "/spellbook/index.html"
+  },
+  {
+    "revision": "0ff63a4417df16d1a45f",
+    "url": "/spellbook/static/css/main.e2c3f6fe.chunk.css"
+  },
+  {
+    "revision": "e2bf4bb451f5e0295e77",
+    "url": "/spellbook/static/js/2.e1ec3000.chunk.js"
+  },
+  {
+    "revision": "0ff63a4417df16d1a45f",
+    "url": "/spellbook/static/js/main.02bab954.chunk.js"
+  },
+  {
+    "revision": "1fe9893428ffd8c645ec",
+    "url": "/spellbook/static/js/runtime~main.86bdc991.js"
+  }
+]);
